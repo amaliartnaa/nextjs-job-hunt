ebb153595d68a3c62f90e49c4bae43af8dcc265d
amaliartnaa <amaliaratna41@gmail.com>
2024-09-13 11:27:08 +0700

    feat(slicing): add reusable form filter 2

--- src/app/find-jobs/page.tsx
+++ src/app/find-jobs/page.tsx
@@ -1,8 +1,17 @@
 import ExploreDataContainer from '@/containers/ExploreDataContainer'
+import { formFilterSchema } from '@/lib/form-schema'
 import React from 'react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import {zodResolver} from '@hookform/resolvers/zod';
 
 export default function FindJobsPage() {
+  const formFilter = useForm<z.infer<typeof formFilterSchema>>({
+    resolver: zodResolver(formFilterSchema)
+  })
+
+  const onSubmitFormFilter = async (val: z.infer<typeof formFilterSchema>) => console.log(val);
   return (
-    <ExploreDataContainer />
+    <ExploreDataContainer formFilter={formFilter} onSubmitFilter={onSubmitFormFilter}/>
   )
 }
